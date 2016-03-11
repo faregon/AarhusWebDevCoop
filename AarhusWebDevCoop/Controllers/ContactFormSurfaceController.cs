c068e5af8b56c747d3f12f138bdc868a213b0c12
Faregon <kunobelin@yahoo.com>
2016-03-11 10:02:02 +0100

    first commit

---
+++ AarhusWebDevCoop/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,64 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using AarhusWebDevCoop.ViewModels;
+using System.Net.Mail;
+using Umbraco.Core.Models;
+
+namespace AarhusWebDevCoop.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+        {
+        // GET: ContactFormSurface
+
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+            if (!ModelState.IsValid) { return CurrentUmbracoPage(); }
+            MailMessage message = new MailMessage();
+            message.To.Add("minko.todorov@gmail.com");
+            message.Subject = model.Subject;
+            message.From = new MailAddress(model.Email, model.Name);
+            message.Body = model.Message;
+
+
+            using (SmtpClient smtp = new SmtpClient())
+            {
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+                smtp.EnableSsl = true;
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.Credentials = new System.Net.NetworkCredential("minko.todorov@gmail.com", "ekvitwkevwifxkbg");
+                smtp.EnableSsl = true;
+
+                smtp.Send(message);
+            }
+            // Parameters – name, parentId, contentTypeAlias
+            IContent comment = Services.ContentService.CreateContent(model.Subject, CurrentPage.Id, "Message");
+
+            // assign values
+            comment.SetValue("messageName", model.Name);
+            comment.SetValue("email", model.Email);
+            comment.SetValue("subject", model.Subject);
+            comment.SetValue("message", model.Message);
+            // save to Umbraco
+            Services.ContentService.Save(comment);
+            //Services.ContentService.SaveAndPublish(comment);
+
+
+            TempData["Success"] = true;
+
+   
+
+            return RedirectToCurrentUmbracoPage();
+
+
+        }
+        
+
+    }
+}
---
+++ AarhusWebDevCoop/ViewModels/ContactForm.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AarhusWebDevCoop.ViewModels
+{
+    public class ContactForm
+    {
+        [Required(ErrorMessage = "Please fill your name")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage ="This is not a valid email adress")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please specify a Subject")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "DOn't just send me empty messages")]
+        public string Message { get; set; }
+    }
+}
